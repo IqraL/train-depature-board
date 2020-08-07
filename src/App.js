d9ae37b2182a241b116609083726329e12b18422
IqraL <IqraLatif159@gmail.com>
2020-08-07 18:59:24 +0100

    gql setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import Board from "./Board";
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Board />
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ src/Board.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+function Board() {
+  return <div></div>;
+}
+
+export default Board;

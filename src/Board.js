728b204540ba49c5d770f0cdfea8ab0349c26a9e
IqraL <IqraLatif159@gmail.com>
2020-08-08 18:32:12 +0100

    added seach button and centred components

--- src/Board.js
+++ src/Board.js
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
 import TrainStationDropDown from "./TrainStationDropDown";
+import Button from "@material-ui/core/Button";
 const boardWrapper = {
   display: "grid",
   justifyContent: "center",
   minWidth: "fit-content",
+  paddingTop: "100px",
+  gridRowGap: "50px",
 };
 const selectorHolder = {
   display: "grid",
-  gridTemplateColumns: "1fr 1fr",
+  gridTemplateColumns: "1fr 1fr 0fr",
   gridColumnGap: "50px",
 };
 const headerStyle = {
+  display: "grid",
+  justifyContent: "center",
   textDecoration: "underline",
-  fontSize: "large",
+};
+const selectedWrapper = {
+  display: "grid",
+  gridTemplateColumns: "0fr 1fr",
+  gridColumnGap: "15px",
 };
 function Board() {
   const [depatureStation, setDepatureStation] = useState(null);
@@ -21,20 +30,29 @@ function Board() {
 
   return (
     <div style={boardWrapper}>
-      <div style={headerStyle}>Train Depature Board</div>
+      <div style={headerStyle}>
+        <h2>Train Depature Board</h2>
+      </div>
       <div style={selectorHolder}>
-        <div>
+        <div style={selectedWrapper}>
+          <div>Depature Station :</div>
           <TrainStationDropDown
             name="Depature Station"
             onChange={(value) => setDepatureStation(value)}
           />
         </div>
-        <div>
+        <div style={selectedWrapper}>
+          <div>Destination Station :</div>
           <TrainStationDropDown
             name="Destination Station"
             isDisabled={allDepatures}
             onChange={(value) => setDestinationStation(value)}
           />
+        </div>
+        <div>
+          <Button variant="contained" color="primary">
+            Go
+          </Button>
         </div>
       </div>
     </div>

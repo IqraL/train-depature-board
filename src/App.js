1c41007df84437cf78be0f6006f75e914f62b797
IqraL <IqraLatif159@gmail.com>
2020-08-09 20:28:54 +0100

    added env vars

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from "react";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import Board from "./Board";
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri:
+    process.env.REACT_APP_ENV === "dev"
+      ? process.env.REACT_APP_DEV_URI
+      : process.env.REACT_APP_PRODUCTION_URI,
   cache: new InMemoryCache(),
 });
+
+console.log(process.env.ENV);
 
 function App() {
   return (
